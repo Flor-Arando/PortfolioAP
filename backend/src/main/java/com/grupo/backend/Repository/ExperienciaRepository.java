d4cd0a894fb870b7c577d5349336cd17309fc8b8
Flor-Arando <arando.florencia@gmail.com>
2022-10-05 17:45:10 -0300

    Se elimina backend para dejar solo frontend

--- backend/src/main/java/com/grupo/backend/Repository/ExperienciaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.grupo.backend.Repository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import com.grupo.backend.Model.Experiencia;
-
-public interface ExperienciaRepository extends CrudRepository<Experiencia, Integer> {
-    @Query("SELECT s FROM Experiencia as s WHERE empresa = ?1 AND puesto = ?2 AND desde =?3 AND hasta =?4 AND descripcion = ?5")
-    Experiencia getExperienciaByRegistro(String empresa, String puesto, String desde, String hasta, String descripcion);
-}
