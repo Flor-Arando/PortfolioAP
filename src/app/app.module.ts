5a942b69934e165fc0d1bdf037993d04198a0652
Flor-Arando <arando.florencia@gmail.com>
2022-10-10 12:39:45 -0300

    Drag & drop de elementos seccion sobre mi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'; // https://angular.io/api/common/http/HttpClientModule
+import { HttpClientModule } from '@angular/common/http';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppComponent } from './app.component';
 import { SkillCirculoComponent } from './skill-circulo/skill-circulo.component';
 import { FormsModule } from '@angular/forms';
 import { InicioComponent } from './inicio/inicio.component';
-import { SobreMiComponent } from './sobre-mi/sobre-mi.component'; // <-- NgModel lives here
-import { EducacionComponent } from './educacion/educacion.component'; // <-- NgModel lives here
-import { ProyectosComponent } from './proyectos/proyectos.component'; // <-- NgModel lives here
-import { ExperienciaComponent } from './experiencia/experiencia.component'; // <-- NgModel lives here
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { ExperienciaComponent } from './experiencia/experiencia.component'; // <
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    DragDropModule
   ],
   exports : [
     SkillCirculoComponent
